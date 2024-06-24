d4deb024cada209b0d9be3ba046cd68f0356238f
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-06-24 13:06:30 -0800

    Actually flye_lr-soil.bash

--- code/assembly/flye_lr-soil.bash
+++ code/assembly/flye_lr-soil.bash
@@ -7,7 +7,7 @@
 mkdir -p data/process/flye/lr-soil/logs
 touch data/process/flye/lr-soil/logs/log_flye_lr-soil.txt
 {
-flye -meta --nano-raw data/raw/lr-marine_sediment/SRR27145287/SRR27145287.fastq --out-dir data/process/flye/lr-soil --threads 12
+flye -meta --nano-raw data/raw/lr-soil/SRR21053856/SRR21053856.fastq --out-dir data/process/flye/lr-soil --threads 12
 
 } > data/process/flye/lr-soil/logs/log_flye_lr-soil.txt
 
