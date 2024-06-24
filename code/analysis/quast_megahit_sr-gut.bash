32040ea899f2709f7f42ac1943df1e3638ed86a2
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-06-24 00:44:43 -0800

    Revised flye method

---
+++ code/analysis/quast_megahit_sr-gut.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Script to analyze the quality of the metagenomes
+## Analyzer: METAQUAST
+## Dataset: Short read Gut
+## Uses this dataset: 
+## Assembler: Megahit
---
+++ code/installation/install_quast.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+### Install Dependencies
+conda install pkg-config
