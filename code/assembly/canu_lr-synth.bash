ff83bcec982a2752c3676345ca0564f2d0598422
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-06-24 13:02:55 -0800

    Fixed error in flye_lr-gut

---
+++ code/assembly/canu_lr-synth.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Dataset: Long-read Gut
+## Uses this dataset: https://www.ncbi.nlm.nih.gov/sra/SRX14622818%5Baccn
+## Assembler: Canu
+
+mkdir -p data/process/canu/lr-gut/logs
+touch data/process/canu/lr-gut/logs/log_flye_lr-gut.txt
+{
+.tools/assemblers/canu-2.2/bin/canu -p lr-gut -d data/process/canu/lr-gut/ genomeSize = 5300000 -nanopore data/raw/lr-gut/SRR18491310/SRR18491310.fastq
+
+} > data/process/canu/lr-gut/logs/log_canu_lr-gut.txt
--- code/assembly/flye_lr-soil.bash
+++ code/assembly/flye_lr-soil.bash
@@ -7,7 +7,7 @@
 mkdir -p data/process/flye/lr-soil/logs
 touch data/process/flye/lr-soil/logs/log_flye_lr-soil.txt
 {
-flye --nano-raw data/raw/lr-marine_sediment/SRR27145287/SRR27145287.fastq --out-dir data/process/flye/lr-soil --threads 4
+flye -meta --nano-raw data/raw/lr-marine_sediment/SRR27145287/SRR27145287.fastq --out-dir data/process/flye/lr-soil --threads 12
 
 } > data/process/flye/lr-soil/logs/log_flye_lr-soil.txt
 
