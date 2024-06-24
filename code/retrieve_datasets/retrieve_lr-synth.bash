d83df9660c42eeeab90dee84d140fa3622903c1a
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-06-24 00:52:28 -0800

    added Script for Synthetic Community

---
+++ code/retrieve_datasets/retrieve_lr-synth.bash
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+## Sets working directory to the Long read Synthetic community folder
+## Uses this dataset: https://www.ncbi.nlm.nih.gov/sra/PRJNA903965
+## This uses the SRA toolkit from NCBI to retrieve the reads
+
+mkdir -p data/raw/lr-synth
+cd data/raw/lr-synth
+~/mailroom/sratoolkit.3.1.1-ubuntu64/bin/vdb-config --prefetch-to-cwd
+~/mailroom/sratoolkit.3.1.1-ubuntu64/bin/prefetch SRR22366767
+cd SRR22366767/
+~/mailroom/sratoolkit.3.1.1-ubuntu64/bin/fasterq-dump SRR22366767.sra
