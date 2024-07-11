bafa84d7968003649047317c23892eeeb717f553
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-07-11 10:59:20 -0800

    Megahit assembly: fixed error in scripts, removed mkdir

--- code/assembly/megahit/scripts/megahit_sr-bsc.bash
+++ code/assembly/megahit/scripts/megahit_sr-bsc.bash
@@ -4,6 +4,5 @@
 ## Assembler: MEGAHIT
 
 
-mkdir -p data/process/megahit/sr-bsc
 megahit -1 data/raw/sr-bsc/SRR28765359/SRR28765359_1.fastq -2 data/raw/sr-bsc/SRR28765359/SRR28765359_2.fastq -o data/process/megahit/sr-bsc/
 
--- code/assembly/megahit/scripts/megahit_sr-diabetes.bash
+++ code/assembly/megahit/scripts/megahit_sr-diabetes.bash
@@ -3,5 +3,4 @@
 ## Uses this Database
 ## Assembler: MEGAHIT
 
-mkdir -p data/process/megahit/sr-diabetes/
 megahit -1 data/raw/sr-diabetes/SRR341725/SRR341725_1.fastq -2 data/raw/sr-diabetes/SRR341725/SRR341725_2.fastq -o data/process/megahit/sr-diabetes/
