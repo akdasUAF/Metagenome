47bc26d788f84e4712c91d5a50ea42205707d491
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-07-13 21:59:06 -0800

    Miniasm assembly: removed gfa to fa step

--- code/assembly/miniasm/run_miniasm_lr-ecoli.bash
+++ code/assembly/miniasm/run_miniasm_lr-ecoli.bash
@@ -4,4 +4,4 @@
 
 conda run -n asm_miniasm bash code/assembly/miniasm/scripts/miniasm_lr-ecoli.bash | tee logs/assembly/log_assemble_miniasm_lr-ecoli.log
 
-conda run -n pro_python python3 code/processing/gfa_to_fa.py data/process/miniasm/lr-ecoli/assembly_minimap_lr-ecoli.gfa data/process/miniasm/lr-ecoli/assembly_minimap_lr-ecoli.fa | tee logs/processing/log_gfa_to_fa_miniasm_lr-ecoli.log
+# conda run -n pro_python python3 code/processing/gfa_to_fa.py data/process/miniasm/lr-ecoli/assembly_minimap_lr-ecoli.gfa data/process/miniasm/lr-ecoli/assembly_minimap_lr-ecoli.fa | tee logs/processing/log_gfa_to_fa_miniasm_lr-ecoli.log
