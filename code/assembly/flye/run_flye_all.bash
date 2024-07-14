14506d30039bf2916c661bf73776552c41cc6887
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-07-14 01:54:57 -0800

    Added run all function for assemblies

--- code/assembly/flye/run_flye_all.bash
+++ code/assembly/flye/run_flye_all.bash
@@ -2,11 +2,11 @@
 ## Assembler: flye
 
 ## lr-ecoli
-bash code/assembly/flye/run_flye_lr-ecoli.bash
+bash code/assembly/flye/run_flye_lr-ecoli.bash || echo "Flye lr-ecoli: Failed" | tee -a logs/assembly/log_assemble_canu_lr-soil.log
 ## lr-gut
-bash code/assembly/flye/run_flye_lr-gut.bash
+bash code/assembly/flye/run_flye_lr-gut.bash || echo "Flye lr-gut: Failed" | tee -a logs/assembly/log_assemble_canu_lr-soil.log
 ## lr-marine_sediment
-bash code/assembly/flye/run_flye_lr-marine_sediment.bash
+bash code/assembly/flye/run_flye_lr-marine_sediment.bash || echo "Flye lr-marine_sediment: Failed" | tee -a logs/assembly/log_assemble_canu_lr-soil.log
 ## lr-soil
-bash code/assembly/flye/run_flye_lr-soil.bash
+bash code/assembly/flye/run_flye_lr-soil.bash || echo "Flye lr-soil: Failed" | tee -a logs/assembly/log_assemble_canu_lr-soil.log
 
--- code/assembly/miniasm/run_miniasm_all.bash
+++ code/assembly/miniasm/run_miniasm_all.bash
@@ -2,11 +2,11 @@
 ## Assembler: miniasm
 
 ## lr-ecoli
-bash code/assembly/miniasm/run_miniasm_lr-ecoli.bash
+bash code/assembly/miniasm/run_miniasm_lr-ecoli.bash || echo "Miniasm lr-ecoli: Failed" | tee -a logs/assembly/log_assemble_miniasm_lr-ecoli.log
 ## lr-gut
-bash code/assembly/miniasm/run_miniasm_lr-gut.bash
+bash code/assembly/miniasm/run_miniasm_lr-gut.bash || echo "Miniasm lr-gut: Failed" | tee -a logs/assembly/log_assemble_miniasm_lr-gut.log
 ## lr-marine_sediment
-bash code/assembly/miniasm/run_miniasm_lr-marine_sediment.bash
+bash code/assembly/miniasm/run_miniasm_lr-marine_sediment.bash || echo "Miniasm lr-marine_sediment: Failed" | tee -a logs/assembly/log_assemble_miniasm_lr-marine_sediment.log
 ## lr-soil
-bash code/assembly/miniasm/run_miniasm_lr-soil.bash
+bash code/assembly/miniasm/run_miniasm_lr-soil.bash || echo "Miniasm lr-soil: Failed" | tee -a logs/assembly/log_assemble_miniasm_lr-soil.log
 
