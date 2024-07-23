928bda35c8c0b3aad5d1e8c87c221ea99781af06
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-07-23 13:31:27 -0800

    Added dool for quast

--- code/analysis/quast/canu/run_quast_canu_lr-ecoli.bash
+++ code/analysis/quast/canu/run_quast_canu_lr-ecoli.bash
@@ -3,5 +3,6 @@
 ## Assembler: canu
 ## Dataset: lr-ecoli
 
-
-conda run -n ana_metaquast bash code/analysis/quast/canu/scripts/quast_canu_lr-ecoli.bash | tee logs/analysis/log_quast_canu_lr-ecoli.log
+bash code/analysis/dool/start_dool.bash logs/analysis/dool/quast/log_dool_quast_canu_lr-ecoli.log
+conda run -n ana_metaquast bash code/analysis/quast/canu/scripts/quast_canu_lr-ecoli.bash | tee logs/analysis/log_quast_canu_lr-ecoli.log || echo 'Quast (canu lr-ecoli) failed'
+bash code/analysis/dool/kill_dool.bash
