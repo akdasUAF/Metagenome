7c9d9a988894f39e82c05b681637736d33de742f
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-07-22 23:23:04 -0800

    Added dool test

---
+++ code/analysis/dool/example.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bash start_dool2.bash
+sleep 3
+
+bash kill_dool2.bash
--- code/assembly/canu/run_canu_lr-ecoli_dool.bash
+++ code/assembly/canu/run_canu_lr-ecoli_dool.bash
@@ -2,8 +2,18 @@
 ## Assembler: Canu
 ## Dataset: Long read Ecoli
 
-dool -t -a -d 1 --output system_stats.csv
+# /usr/bin/time -v bash -c "source code/assembly/canu/run_canu_lr-ecoli.bash" 2>&1 | tee -a logs/assembly/log_assemble_canu_lr-ecoli.log
 
-/usr/bin/time -v bash -c "source code/assembly/canu/run_canu_lr-ecoli.bash" 2>&1 | tee -a logs/assembly/log_assemble_canu_lr-ecoli.log
+pid=$(dool -a --output report3.csv &)
+echo "Background dool process started with PID: $pid"
 
-kill -INT $(pgrep dool)
+# Wait for 3 seconds (or adjust the wait time as needed)
+sleep 3
+
+# Check if the process is still running before killing
+if ps -p $pid > /dev/null 2>&1; then
+  echo "Killing dool process (PID: $pid)..."
+  kill $pid
+else
+  echo "dool process (PID: $pid) might have already finished."
+fi
