54b5bb84ff4a1cc782b67a1b274b5f0e72539870
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-07-23 13:03:09 -0800

    Added dool installation scripts

---
+++ code/installation/run_install_dool.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+## Analysis: dool
+
+bash code/installation/scripts/install_dool.bash | tee logs/installation/log_install_dool.log
---
+++ code/installation/scripts/install_dool.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd tools/analysis/
+git clone https://github.com/scottchiefbaker/dool.git
+cd dool/
+python3 ./install.py
