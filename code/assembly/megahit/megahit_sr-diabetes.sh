a735ab5e30a4fd7e6664b4a238ae138694ebe0b9
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-07-30 12:51:08 -0800

    Fixed error where megahit was creating a directory then complaining about it

--- code/assembly/megahit/megahit_sr-diabetes.sh
+++ code/assembly/megahit/megahit_sr-diabetes.sh
@@ -2,5 +2,5 @@
 ## Dataset: sr-diabetes
 ## Assembler: MEGAHIT
 
-mkdir -p data/MAG/megahit/sr-diabetes/
+rm -rf data/MAG/megahit/sr-diabetes/
 megahit -1 data/raw/sr-diabetes/SRR341725/SRR341725_1.fastq -2 data/raw/sr-diabetes/SRR341725/SRR341725_2.fastq -o data/MAG/megahit/sr-diabetes/
