ff2c92625ca2060b319b1cb8444bd06dcb8fc99f
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-01 07:02:24 -0800

    Added slurm megahit sr-bsc

--- code/assembly/megahit/megahit_sr-bsc.sh
+++ code/assembly/megahit/megahit_sr-bsc.sh
@@ -3,7 +3,7 @@
 ## Dataset: sr-bsc
 ## Assembler: megahit
 
-rm -rf data/MAG/megahit/sr-bsc/
+rm -rf data/MAG/megahit/sr-bsc/ || continue
 mkdir -p data/analysis/megahit/sr-bsc/
 
 megahit -1 data/raw/sr-bsc/SRR28765359/SRR28765359_1.fastq -2 data/raw/sr-bsc/SRR28765359/SRR28765359_2.fastq -o data/MAG/megahit/sr-bsc/
