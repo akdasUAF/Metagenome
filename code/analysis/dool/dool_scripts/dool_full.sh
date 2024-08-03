7fc3655f0a06776133af96ab83a04a6eb0472484
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-03 12:23:45 -0800

    Consolidated dool output; changed raven run command

---
+++ code/analysis/dool/dool_scripts/dool_full.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if at least one argument is provided
+if [[ $# -eq 0 ]]; then
+  echo "Error: Please provide a path as an argument."
+  exit 1
+fi
+
+path="$1"
+
+dool -afr --aio --fs --ipc --socket --tcp --unix --vm --vm-adv --zones --cpufreq --disk-avgqu --disk-avgrq --disk-inflight --disk-svctm --disk-util --output "$path" &
+
+
+pid=$!
+echo $pid >> /tmp/dool.pid
+exit 0
