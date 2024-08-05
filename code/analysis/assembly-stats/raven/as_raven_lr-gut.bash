6f3da5d1d836120de114c91a9a0e6f3fb79ea405
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-05 00:41:31 -0800

    Added abyss k-alternatives

--- code/analysis/assembly-stats/raven/as_raven_lr-gut.bash
+++ code/analysis/assembly-stats/raven/as_raven_lr-gut.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Analyzer: assembly-stats
 ## Assembler: raven
-## Dataset: lr-soil
+## Dataset: lr-gut
 
-./tools/analysis/assembly-stats/build/assembly-stats data/MAG/raven/lr-soil/assembly_raven_lr-soil.fasta | tee data/analysis/raven/lr-soil/assembly-stats_raven_lr-soil.txt
+./tools/analysis/assembly-stats/build/assembly-stats data/MAG/raven/lr-gut/assembly_raven_lr-gut.fasta | tee data/analysis/raven/lr-gut/assembly-stats_raven_lr-gut.txt
