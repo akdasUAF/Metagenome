5ab65a4d5e39df9c63e14b55d95527c38185e57d
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 01:54:31 -0800

    megahit ms

---
+++ code/assembly/megahit/run_megahit_sr-ms.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+
+bash code/assembly/megahit/run_megahit.sh data/process/sr-bsc/ data/MAG/sr-bsc/
--- code/assembly/megahit/run_scripts/megahit_sr-bsc.sh
+++ code/assembly/megahit/run_scripts/megahit_sr-bsc.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-bash code/assembly/megahit/run_megahit_with_options.sh data/process/sr-bsc/trimmed/ data/MAG/sr-bsc/ data/analysis/sr-bsc/
+
+bash code/analysis/dool/start_dool.sh "$path_analysis"/dool_asm_megahit_sr-bsc.csv
+
+conda run -n asm_megahit bash -c "source code/analysis/time/run_with_time.sh code/assembly/megahit/run_megahit_sr-ms.bash" | tee data/analysis/megahit/sr-bsc/log_assemble_megahit_sr-bsc.log
+
+bash code/analysis/dool/kill_dool.sh
