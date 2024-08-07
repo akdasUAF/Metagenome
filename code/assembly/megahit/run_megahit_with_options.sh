ca08b17ff37c30391da27afbb51684db30f3a0e7
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 01:44:31 -0800

    Fixed time keeping tracking

--- code/assembly/megahit/run_megahit_with_options.sh
+++ code/assembly/megahit/run_megahit_with_options.sh
@@ -8,14 +8,17 @@ fi
 path_reads=$1
 path_output=$2
 path_analysis=$3
-bash_to_run="code/assembly/megahit/run_megahit.sh ""$path_reads" "$path_output"
+
+script_name="code/assembly/megahit/run_megahit.sh"
+bash_to_run="$script_name ""$path_reads" "$path_analysis"
 
 
 rm -rf $path_output
 mkdir -p path_analysis
 
+
 bash code/analysis/dool/start_dool.sh "$path_analysis"/dool_asm_megahit_sr-bsc.csv
 
-conda run -n asm_megahit bash -c "source code/analysis/time/run_with_time.sh $bash_to_run" | tee data/analysis/megahit/sr-bsc/log_assemble_megahit_sr-bsc.log
+conda run -n asm_megahit bash -c "source code/analysis/time/run_with_time.sh "$bash_to_run"" | tee data/analysis/megahit/sr-bsc/log_assemble_megahit_sr-bsc.log
 
 bash code/analysis/dool/kill_dool.sh
--- code/multistep/send_fastqc.bash
+++ code/multistep/send_fastqc.bash
@@ -3,5 +3,11 @@ scp wwinnett@137.229.25.190:metagenome_replicates/full_datasets/Metagenome/data/
 
 
 scp wwinnett@137.229.25.190:metagenome_replicates/full_datasets/Metagenome/data/analysis/sr-bsc/trimmed_fastqc_round2/fastqc/SRR*/*.html ../Moving/fastqc/round_2
+
+scp wwinnett@137.229.25.190:metagenome_replicates/full_datasets/Metagenome/data/analysis/sr-ms/fastqc/SRR*/*.html ../Moving/fastqc/sr-ms/round1
+
+
 mkdir ../Moving/fastqc/round_2
 
+
+  trimmomatic PE -threads 24 data/raw/sr-ms/forward/SRR27456520_1.fastq data/raw/sr-ms/reverse/SRR27456520_2.fastq data/process/sr-ms/trimmed/sr-ms_trimmed_SRR27456520_1.fastq data/process/sr-ms/trimmed/sr-ms_trimmed_SRR27456520_1.fastq.unpaired data/process/sr-ms/trimmed/sr-ms_trimmed_SRR27456520_2.fastq data/process/sr-ms/trimmed/sr-ms_trimmed_SRR27456520_2.fastq.unpaired LEADING:5 TRAILING:5 SLIDINGWINDOW:4:15 MINLEN:36 
