e8079fae65dcbbd2dd0043829b9369fcfd0ed601
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 11:24:31 -0800

    Changed conda hook on megahit sr-ms

--- code/assembly/megahit/run_scripts/megahit_sr-ms.sh
+++ code/assembly/megahit/run_scripts/megahit_sr-ms.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bash code/assembly/megahit/run_megahit_with_options.sh data/process/sr-ms/ data/MAG/sr-ms/ data/analysis/megahit/sr-ms/
+# conda run -n asm-megahit bash -c "source code/assembly/megahit/run_megahit_with_options.sh data/process/sr-ms/ data/MAG/sr-ms/ data/analysis/megahit/sr-ms/"
+conda run -n asm-megahit bash -c "code/assembly/megahit/run_megahit_with_options.sh data/process/sr-ms/ data/MAG/sr-ms/ data/analysis/megahit/sr-ms/"
