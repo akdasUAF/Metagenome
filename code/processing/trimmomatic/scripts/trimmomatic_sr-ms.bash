3fe6cc4c3e830d3a4fa6a86bdce40082d8e93986
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 01:22:25 -0800

    Added a prmer free trimmomatic

--- code/processing/trimmomatic/scripts/trimmomatic_sr-ms.bash
+++ code/processing/trimmomatic/scripts/trimmomatic_sr-ms.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash code/processing/trimmomatic/run_trimmomatic_basic.bash data/raw/sr-ms/ data/process/sr-ms/
+conda run -n pro_trimmomatic bash code/processing/trimmomatic/trimmomatic_basic.bash data/raw/sr-ms/ data/process/sr-ms/
