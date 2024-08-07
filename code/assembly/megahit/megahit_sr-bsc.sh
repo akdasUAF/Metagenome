93d1b5945f4092a7fe6fd28513b2efbbf6ec632d
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-06 22:16:50 -0800

    Added code assembly megahit

--- code/assembly/megahit/megahit_sr-bsc.sh
+++ code/assembly/megahit/megahit_sr-bsc.sh
@@ -6,5 +6,5 @@
 rm -rf data/MAG/megahit/sr-bsc/ || continue
 mkdir -p data/analysis/megahit/sr-bsc/
 
-megahit -1 data/process/sr-bsc/sr-bsc_trimmed_1.fastq -2 data/process/sr-bsc/sr-bsc_trimmed_2.fastq -o data/MAG/megahit/sr-bsc/ --min-contig-len 1000
+megahit -1 data/process/sr-bsc/trimmed/*_1.fastq -2 data/process/sr-bsc/trimmed/*_2.fastq -o data/MAG/megahit/ --min-contig-len 1000
 
--- code/multistep/send_fastqc.bash
+++ code/multistep/send_fastqc.bash
@@ -2,4 +2,6 @@
 scp wwinnett@137.229.25.190:metagenome_replicates/full_datasets/Metagenome/data/analysis/sr-bsc/fastqc/SRR*/*.html ../Moving/fastqc/
 
 
-scp wwinnett@137.229.25.190:metagenome_replicates/full_datasets/Metagenome/data/analysis/sr-bsc/fastqc/SRR*/*.html ../Moving/fastqc/
+scp wwinnett@137.229.25.190:metagenome_replicates/full_datasets/Metagenome/data/analysis/sr-bsc/trimmed_fastqc_round2/fastqc/SRR*/*.html ../Moving/fastqc/round_2
+mkdir ../Moving/fastqc/round_2
+
