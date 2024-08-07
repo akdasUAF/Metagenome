e84816734fbecd5fa0dd838d53c41160b16cb079
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-06 20:55:01 -0800

    Added fastqc

--- code/analysis/fastqc/run_fastqc.bash
+++ code/analysis/fastqc/run_fastqc.bash
@@ -11,15 +11,15 @@ fastqc_output="$path_analysis"/fastqc
 
 mkdir -p "$fastqc_output"
 
-for file in "$raw_dir"forward/*_1.fastq; do
-  base_name=$(basename "$file" _1.fastq)
-  output_dir="$fastqc_output/$base_name"
-  mkdir -p "$output_dir"
+# for file in "$raw_dir"forward/*_1.fastq; do
+#   base_name=$(basename "$file" _1.fastq)
+#   output_dir="$fastqc_output/$base_name"
+#   mkdir -p "$output_dir"
 
-  fastqc "$file" -o "$output_dir"
-done
+#   fastqc "$file" -o "$output_dir"
+# done
 
-for file in "$raw_dir"backward/*_2.fastq; do
+for file in "$raw_dir"reverse/*_2.fastq; do
   base_name=$(basename "$file" _2.fastq)
   output_dir="$fastqc_output/$base_name"
   mkdir -p "$output_dir"
