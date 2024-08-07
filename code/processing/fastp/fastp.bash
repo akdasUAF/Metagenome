c982fdfcf64bb15d7cb033a0b15f0bc753e95cf6
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 15:03:31 -0800

    Increased -n 5 -> -n 10

--- code/processing/fastp/fastp.bash
+++ code/processing/fastp/fastp.bash
@@ -19,8 +19,6 @@ for read_id in $(ls "$path_raw"forward/*.fastq | cut -d '/' -f 5 | cut -d '_' -f
   output_paired_2="${output_prefix}_2.fast.gz"
   output_report="${path_analysis}/${read_id}_report.html"
 
-  fastp -i "$forward_file" -I "$reverse_file" -o "$output_paired_1" -O "$output_paired_2" -h "$output_report" -q 20 -p 95 -l 30 -n 5
+  fastp -i "$forward_file" -I "$reverse_file" -o "$output_paired_1" -O "$output_paired_2" -h "$output_report" -q 20 -p 95 -l 30 -n 10
 done
-
-
 
