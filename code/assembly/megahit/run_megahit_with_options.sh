5061584692c142eb5579edbb79f22d18e501187e
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 17:04:28 -0800

    Fixed fastp

--- code/assembly/megahit/run_megahit_with_options.sh
+++ code/assembly/megahit/run_megahit_with_options.sh
@@ -18,25 +18,12 @@ bash code/analysis/dool/start_dool.sh "$path_analysis"/dool_asm_megahit_sr-bsc.c
 
 time_output=$(/usr/bin/time -v bash -c "$command_to_run" 2>&1)
 
-# Call run_with_time to capture execution time
-# time_output=$(./code/analysis/time/run_with_time.sh "$script_name" "$path_reads" "$path_output")
-
 # Check for script execution error (optional)
 if [ $? -ne 0 ]; then
   echo "Error: Script '$script_name' failed to execute."
   echo "$time_output" > time_output.txt  # Optional: Save error output
   exit 1
 fi
-
-
-
-# dataset=$(basename "$path_output")
-# log_file="$path_analysis/log_assemble_megahit_${dataset}"
-
-
-# # conda run -n asm_megahit bash -c "source code/analysis/time/run_with_time.sh "$bash_to_run"" | tee data/analysis/megahit/sr-bsc/log_assemble_megahit_sr-bsc.log
-# conda run -n asm_megahit bash -c "source code/analysis/time/run_with_time.sh $script_name '$path_reads' '$path_output'" > "$log_file"
-
 
 bash code/analysis/dool/kill_dool.sh
 
--- code/processing/fastp/fastp.bash
+++ code/processing/fastp/fastp.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-  echo "Usage: $0 <raw_directory> <trimmed_sequences_out_directory> <analysis_directory> <number of >"
+  echo "Usage: $0 <raw_directory> <trimmed_sequences_out_directory> <analysis_directory>"
   exit 1
 fi
 
@@ -15,11 +15,21 @@ mkdir -p "$path_output""trimmed/"
 for read_id in $(ls "$path_raw"forward/*.fastq | cut -d '/' -f 5 | cut -d '_' -f 1); do
   forward_file="$path_raw""forward/${read_id}_1.fastq"
   reverse_file="$path_raw""/reverse/${read_id}_2.fastq"
+  
+  mkdir -p "$path_output/trimmed/"
+  mkdir -p "${path_analysis}/fastp/"
+
   output_prefix="$path_output""trimmed/sr-bsc_trimmed_${read_id}"
   output_paired_1="${output_prefix}_1.fastq.gz"
   output_paired_2="${output_prefix}_2.fastq.gz"
-  output_report="${path_analysis}/${read_id}/fastp${read_id}_report.html"
+  output_report="${path_analysis}/fastp/fastp_${read_id}_report.html"
 
   fastp -i "$forward_file" -I "$reverse_file" -o "$output_paired_1" -O "$output_paired_2" -h "$output_report" -q 20 -p 95 -l 30 -n 10
+
+   # Check for Fastp errors
+  if [ $? -ne 0 ]; then
+    echo "Error running Fastp for file: $forward_file"
+  fi
+
 done
 
