afc1cbe681bf41bb02e6fcc75cbb879ca800c220
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-08 14:56:00 -0800

    Added debug to determine how many reads there are

--- code/assembly/megahit/megahit.sh
+++ code/assembly/megahit/megahit.sh
@@ -15,5 +15,14 @@ rm -rf $path_output
 forward_reads=($(find "$path_reads" -name "*_1.fastq"))
 reverse_reads=($(find "$path_reads" -name "*_2.fastq"))
 
+if [[ ${#forward_reads[@]} -ne ${#reverse_reads[@]} ]]; then
+  echo "Error: Unequal number of forward and reverse reads. Please check your data."
+
+  # Echo the number of forward and reverse reads
+  echo "Number of forward reads: ${#forward_reads[@]}"
+  echo "Number of reverse reads: ${#reverse_reads[@]}"
+  exit 1
+fi
+
 megahit -t 24 -1 $forward_reads -2 $reverse_reads -o $path_output --min-contig-len 1000 2>&1 | tee "${path_log}"
 
