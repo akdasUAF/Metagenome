58f936576782c7c7bdac737fcaf6b1b9489fcf3f
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 22:25:19 -0800

    Fastp

--- code/processing/fastp/scripts/fastp_sr-ms.bash
+++ code/processing/fastp/scripts/fastp_sr-ms.bash
@@ -5,5 +5,6 @@ fastp_script="code/processing/fastp/run_fastp.bash"
 raw_dir="data/raw/sr-ms/"
 trimmed_dir="data/process/sr-ms/"
 analysis_dir="data/analysis/sr-ms/"
+bash_to_run="$fastp_script" "$raw_dir" "$trimmed_dir" "$analysis_dir"
 
-"$benchmark_script" "$fastp_script" "$raw_dir" "$trimmed_dir" "$analysis_dir" -d sr-ms -t fastp
+bash "$benchmark_script" "$bash_to_run" -d sr-ms -t fastp
