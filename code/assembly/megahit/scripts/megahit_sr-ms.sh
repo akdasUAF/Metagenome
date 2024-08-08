e5b07f82c36cd1ea7e59f17c8841f8e914b5755f
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 23:40:41 -0800

    Megahit sr-ms

--- code/assembly/megahit/scripts/megahit_sr-ms.sh
+++ code/assembly/megahit/scripts/megahit_sr-ms.sh
@@ -3,12 +3,17 @@ benchmark_script="code/benchmarking/benchmark.bash"
 path_megahit="code/processing/megahit/run_megahit.bash"
 raw_dir="data/raw/sr-ms/"
 MAG_output="data/process/sr-ms/"
-path_log="data/logs/sr-ms/megahit/log_asm_megahit_sr-ms.sh"
+path_log="data/logs/sr-ms/megahit/"
 dataset="sr-ms"
 task="megahit"
 
+
+mkdir -p ${path_log}
+log_file="${path_log}/log_asm_${task}_${dataset}.log"
+
+
 # Construct the command to be executed
-command="$path_megahit $raw_dir $MAG_output $path_log"
+command="$path_megahit $raw_dir $MAG_output $log_file"
 
 # Execute the benchmark script with the constructed command
 "$benchmark_script" "$command" -d "$dataset" -t "$task"
