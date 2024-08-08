cd473b2727adac02537e4064808e4bf1acc5dc6d
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-07 20:57:07 -0800

    Added perf installer

---
+++ code/install_perf.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define list of environment names as an array
+env_names=("pro_fastp" "asm_abyss" "asm_canu" "asm_flye" "asm_megahit" "asm_metamdbg" "asm_metaspades" "asm_metavelvet" "asm_miniasm" "asm_raven" "ana_assembly-stats" "ana_busco" "ana_metaquast" "ana_jellyfish" "pro_fetch" "pro_trimmomatic" "pro_fastqc" "pro_gfastats" "sra_toolkit")
+
+# Loop through the list
+for env_name in "${env_names[@]}"; do
+  # Check if environment exists (using silent grep)
+  if ! conda env list | grep -q "$env_name"; then
+    echo "Creating environment: $env_name"
+    conda activate "$env_name" # Add any base package specifications here
+	bash code/installation/install_perf.bash
+  else
+    echo "Environment '$env_name' already exists."
+  fi
+done
---
+++ code/installation/benchmarking_install.bash
@@ -0,0 +1,3 @@
+
+# Install linux-perf into every 
+conda install -c conda-forge linux-perf
