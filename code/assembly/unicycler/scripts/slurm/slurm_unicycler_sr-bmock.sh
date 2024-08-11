bcc3c296459fdb4f68dd0c6296f8f2a405cf862d
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-11 00:25:52 -0800

    Added slurm for unicycler sr-bmock

--- code/assembly/unicycler/scripts/slurm/slurm_unicycler_sr-bmock.sh
+++ code/assembly/unicycler/scripts/slurm/slurm_unicycler_sr-bmock.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-benchmark_script="code/benchmarking/benchmark.bash"
-path_unicycler="code/assembly/unicycler/scripts/unicycler_sr-bmock.sh"
-raw_dir="data/process/sr-bmock/trimmed/"
-MAG_output="data/MAG/unicycler/sr-bmock/"
-path_log="data/logs/sr-bmock/unicycler/"
-dataset="sr-bmock"
-task="unicycler"
+#SBATCH --partition=t1standard
+#SBATCH --ntasks=24
+#SBATCH --tasks-per-node=24
+#If running on the bio or analysis queue add:
+###SBATCH --mem=214G
+#SBATCH --mail-user=wwinnett@alaska.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --job-name="panmeta_asm_raven_lr-ecoli_t1standard"
+#SBATCH --output=data/analysis/raven/lr-ecoli/log_slurm_raven_lr-ecoli_%j.log
+#SBATCH --error=data/analysis/raven/lr-ecoli/log_slurm_raven_lr-ecoli_err_%j.log
 
+## Clean out any modules, then reload slurm
+# Since conda will be used, no other modules will be loaded
+module purge
+module load slurm
 
-mkdir -p ${path_log}
-log_file="${path_log}/log_asm_${task}_${dataset}.log"
+ulimit -l unlimited
 
-#  $MAG_output $log_file
-
-# Construct the command to be executed
-command="conda run -n asm_unicycler bash -c "source $path_unicycler"" | tee $log_files
-
-# Execute the benchmark script with the constructed command
-"$benchmark_script" "$command" -d "$dataset" -t "$task"
-
+eval "$(conda shell.bash hook)"
+bash code/assembly/unicycler/scripts/slurm/unicycler_sr-bmock.sh | tee -a data/analysis/sr-bmock/unicycler/log_slurm_assemble_unicycler_sr-bmock_slurm.log || echo 'Assembly failed : Unicycler sr-bmock'
---
+++ code/assembly/unicycler/scripts/slurm/unicycler_sr-bmock.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+benchmark_script="code/benchmarking/benchmark.bash"
+path_unicycler="code/assembly/unicycler/scripts/unicycler_sr-bmock.sh"
+raw_dir="data/process/sr-bmock/trimmed/"
+MAG_output="data/MAG/unicycler/sr-bmock/"
+path_log="data/logs/sr-bmock/unicycler/"
+dataset="sr-bmock"
+task="unicycler"
+
+
+mkdir -p ${path_log}
+log_file="${path_log}/log_asm_${task}_${dataset}.log"
+
+#  $MAG_output $log_file
+
+# Construct the command to be executed
+command="conda run -n asm_unicycler bash -c "source $path_unicycler"" | tee $log_files
+
+# Execute the benchmark script with the constructed command
+"$benchmark_script" "$command" -d "$dataset" -t "$task"
+
