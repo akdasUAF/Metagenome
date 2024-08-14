1f846d9266c3a2b298130518301226c401f78307
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-14 00:46:58 -0800

    added raven lr-ms: Rerouted pipeline

--- code/assembly/raven/run_raven.sh
+++ code/assembly/raven/run_raven.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 <raw_directory> <MAG_out_dir> <path_log_file>"
-  exit 1
-fi
+# if [ $# -ne 3 ]; then
+#   echo "Usage: $0 <raw_directory> <MAG_out_dir> <path_log_file>"
+#   exit 1
+# fi
 
-path_raw=$1
-path_MAG=$2
-path_log_file=$3
+# path_raw=$1
+# path_MAG=$2
+# path_log_file=$3
 
-bash_to_run="code/assembly/raven/run_scripts/run_raven_lr-bd.sh"
+bash_to_run="./tools/assemblers/raven/build/bin/raven -t 24 data/raw/lr-bd/SRR22366767/SRR22366767.fastq > data/MAG/lr-bd/raven/assembly_raven_lr-ecoli.fasta 2> data/analysis/lr-bd/log_assemble_raven_lr-ecoli.log"
+
 echo "${bash_to_run}"
 
 conda run -n asm_raven bash $bash_to_run
