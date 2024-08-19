4d39d174bf120735a83c2fb55f714e45e87ac40d
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-18 17:24:37 -0800

    Fixed assstats to use AS instead of Busco

--- code/analysis/assembly-stats/run_assembly-stats.sh
+++ code/analysis/assembly-stats/run_assembly-stats.sh
@@ -8,7 +8,7 @@ fi
 path_contigs=$1
 path_log=$2
 
-bash_to_run="code/analysis/assembly-stats/busco.bash $path_contigs $path_log"
+bash_to_run="code/analysis/assembly-stats/assembly-stats.sh $path_contigs $path_log"
 echo "Running: ${bash_to_run}........"
 
 conda run -n ana_assembly-stats bash $bash_to_run
