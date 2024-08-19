6393335a4f9e0b60b88212ea2fbccd9d2a3e06b6
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-18 17:35:01 -0800

    Removed metaspades call in busco

--- code/analysis/busco/run_busco.bash
+++ code/analysis/busco/run_busco.bash
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 if [ $# -ne 3 ]; then
-  echo "Usage: $0 <raw_directory> <MAG_out_dir> <path_log_file>"
+  echo "Usage: $0 <path_contigs> <path_analysis> <path_log_file>"
   exit 1
 fi
 
@@ -9,7 +9,7 @@ path_contigs=$1
 path_output=$2
 path_log_file=$3
 
-bash_to_run="code/assembly/metaspades/metaspades.sh $path_contigs $path_output $path_log_file"
+bash_to_run="code/analysis/busco/busco.bash $path_contigs $path_output $path_log_file"
 echo "${bash_to_run}"
 
 conda run -n ana_busco $bash_to_run
