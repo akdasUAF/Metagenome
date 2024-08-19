d81cf9a4c204fe7d1c1846134b0214580c53e746
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-19 12:48:33 -0800

    removed pipeline for abyss

--- code/assembly/abyss/abyss.sh
+++ code/assembly/abyss/abyss.sh
@@ -24,8 +24,12 @@ echo "kmer: $kmer"
 echo "bloom: $bloom"
 echo "    "
 
-abyss_pe_in="${forward_reads}_${reverse_reads}"
+# abyss_pe_in="${forward_reads}_${reverse_reads}"
 
-abyss-pe k=$kmer name=$name_assembly B=$bloom j = 24 \
-	in=$abyss_pe_in \
-	out=$path_output 2>&1 | tee "${path_log}"
+# abyss-pe k=$kmer name=$name_assembly B=$bloom j=24 \
+# 	in=$abyss_pe_in \
+# 	out=$path_output 2>&1 | tee "${path_log}"
+
+
+abyss-pe k=117 name=sr-bmock_abyss B=10G j=24 in='data/process/sr-bmock/trimmed/sr-bmock_trimmed_1.fastq data/process/sr-bmock/trimmed/sr-bmock_trimmed_2.fastq' \
+out='data/MAG/sr-bmock/abyss/' 2>&1 | tee "${path_log}
