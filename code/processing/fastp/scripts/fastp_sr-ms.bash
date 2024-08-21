55fb254fdbd101e279f09fc789ada7ae5117fde4
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-21 13:09:12 -0800

    Added fastp: changed paths on raw dir

--- code/processing/fastp/scripts/fastp_sr-ms.bash
+++ code/processing/fastp/scripts/fastp_sr-ms.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 fastp_script="code/processing/fastp/run_fastp.bash"
 raw_dir="data/raw/sr-ms/"
-forward_path="data/sr-ms/raw/DRR090555/DRR090555_1.fastq"
-reverse_path="data/sr-ms/raw/DRR090555/DRR090555_2.fastq"
+forward_path="data/sr-ms/raw/DRR090555_1.fastq"
+reverse_path="data/sr-ms/raw/DRR090555_2.fastq"
 forward_trimmed_path="data/sr-ms/trimmed/sr-ms_trimmed_1.fastq"
 reverse_trimmed_path="data/sr-ms/trimmed/sr-ms_trimmed_2.fastq"
 
