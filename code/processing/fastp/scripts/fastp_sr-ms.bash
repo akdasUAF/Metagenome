71a007d932072d249d1092b919a893895ec4feb7
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-21 13:07:59 -0800

    Added fastp: changed raw dir

--- code/processing/fastp/scripts/fastp_sr-ms.bash
+++ code/processing/fastp/scripts/fastp_sr-ms.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 fastp_script="code/processing/fastp/run_fastp.bash"
 raw_dir="data/raw/sr-ms/"
-forward_path="data/sr-ms/DRR090555/DRR090555_1.fastq"
+forward_path="data/sr-ms/raw/DRR090555/DRR090555_1.fastq"
 reverse_path="data/sr-ms/raw/DRR090555/DRR090555_2.fastq"
 forward_trimmed_path="data/sr-ms/trimmed/sr-ms_trimmed_1.fastq"
 reverse_trimmed_path="data/sr-ms/trimmed/sr-ms_trimmed_2.fastq"
@@ -16,7 +16,7 @@ task="raven"
 
 mkdir -p $(dirname $path_report)
 mkdir -p ${log_dir}
-log_file="${log_dir}/log_asm_${task}_${dataset}.log"
+log_file="${log_dir}/log_fastp_${dataset}.log"
 
 # Construct the command to be executed
 command="$fastp_script $forward_path $reverse_path $forward_trimmed_path $reverse_trimmed_path $path_report $log_file"
