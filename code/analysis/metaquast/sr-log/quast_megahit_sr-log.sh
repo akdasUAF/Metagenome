d3c99a1a9e82d52b173f2c6d9f450a1eac7d2de1
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-21 18:21:34 -0800

    Addded debug

--- code/analysis/metaquast/sr-log/quast_megahit_sr-log.sh
+++ code/analysis/metaquast/sr-log/quast_megahit_sr-log.sh
@@ -17,7 +17,6 @@ path_reference="code/analysis/metaquast/sr-bmock/sr-bmock_reference_paths.txt"
 
 ### 
 references="$(cat $path_reference)"
-echo $references
 
 quast_output=${path_output}/quast/${assembler}
 path_log="${path_output}/log/log_quast_${assembler}_${dataset}.log"
@@ -28,10 +27,12 @@ mkdir -p $path_output/log/
 
 echo $path_contigs
 echo $quast_output
+echo $references
 echo $path_log
 
 # Construct the command to be executed
-command="$path_megahit $path_contigs $quast_output '$references' $path_log"
+command="$path_megahit $path_contigs $quast_output $references $path_log"
+echo $command
 
 # Execute the benchmark script with the constructed command
 bash $benchmark_script "$command" $dataset $assembler
