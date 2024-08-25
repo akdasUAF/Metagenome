aa2f93804667854ff4b883a02781a23d78a6b2fe
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-25 10:17:20 -0800

    Added sr-log processing

--- code/processing/fastp/scripts/fastp_sr-log.bash
+++ code/processing/fastp/scripts/fastp_sr-log.bash
@@ -1,22 +1,19 @@
 #!/bin/env bash
 fastp_script="code/processing/fastp/run_fastp.bash"
-forward_path="data/sr-log/raw/DRR090555_1.fastq"
-reverse_path="data/sr-log/raw/DRR090555_2.fastq"
+forward_path="data/sr-log/raw/sr-log_forward.fastq"
+reverse_path="data/sr-log/raw/sr-log_reverse.fastq"
 forward_trimmed_path="data/sr-log/trimmed/sr-log_trimmed_1.fastq"
 reverse_trimmed_path="data/sr-log/trimmed/sr-log_trimmed_2.fastq"
 
 path_report="data/sr-log/analysis/sr-log_fastp_report.html"
-log_dir="data/sr-log/log/"
-dataset="sr-log"
-task="fastp"
-
+log_dir="data/sr-log/logs/"
 dataset="sr-log"
 task="fastp"
 
 mkdir -p $(dirname $forward_trimmed_path)
 mkdir -p $(dirname $path_report)
 mkdir -p ${log_dir}
-log_file="${log_dir}/log_fastp_${dataset}.log"
+log_file="${log_dir}/log_${task}_${dataset}.log"
 
 # Construct the command to be executed
 command="$fastp_script $forward_path $reverse_path $forward_trimmed_path $reverse_trimmed_path $path_report $log_file"
