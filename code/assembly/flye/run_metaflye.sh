1c6393b8a3746b86ebeb77fd2a47913ad270a7ba
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-27 14:53:13 -0800

    Added metaflye debug

--- code/assembly/flye/run_metaflye.sh
+++ code/assembly/flye/run_metaflye.sh
@@ -6,11 +6,11 @@ if [ $# -ne 3 ]; then
 fi
 
 path_raw=$1
-path_MAG=$2
+path_output=$2
 path_log_file=$3
 
 echo "path_raw: $path_raw"
-echo "path_MAG: $path_MAG"
+echo "path_output: $path_output"
 echo "path_log_file: $path_log_file"
 
 bash_to_run="code/assembly/flye/metaflye.sh ${path_raw} ${path_output}"
