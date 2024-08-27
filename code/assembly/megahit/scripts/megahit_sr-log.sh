746b7ef6c756d93a66ab2df19af101f783704734
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-27 11:58:15 -0800

    Added megahit_srlog

--- code/assembly/megahit/scripts/megahit_sr-log.sh
+++ code/assembly/megahit/scripts/megahit_sr-log.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 benchmark_script="code/benchmarking/benchmark.bash"
 path_megahit="code/assembly/megahit/run_megahit.sh"
-raw_dir="data/sr-log/trimmed/"
-MAG_output="data/sr-log/megahit/"
-path_log="data/sr-log/log/"
+forward_reads="data/sr-log/trimmed/sr-log_trimmed_1.fastq"
+reverse_reads="data/sr-log/trimmed/sr-log_trimmed_2.fastq"
+path_output="data/sr-log/megahit/"
+path_log="data/sr-log/log/megahit/"
 dataset="sr-log"
 task="megahit"
+
 
 mkdir -p ${path_log}
 log_file="${path_log}/log_asm_${task}_${dataset}.log"
 
 
 # Construct the command to be executed
-command="$path_megahit $raw_dir $MAG_output $log_file"
+command="$path_megahit $forward_reads $reverse_reads $path_output $log_file"
 
 # Execute the benchmark script with the constructed command
 "$benchmark_script" "$command" -d "$dataset" -t "$task"
--- code/retrieve_datasets/scripts/retrieve_sr-bcs.sh
+++ code/retrieve_datasets/scripts/retrieve_sr-bcs.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/bin/env bash
 
-bash code/retrieve_datasets/run_retrieve_sra.bash code/retrieve_datasets/sra_lists/sra-list_sr-bsc.txt data/raw/sr-bsc | tee logs/retrieve_datasets/log_retrieve_lr-ms.log
+datasets="code/retrieve_datasets/sra_lists/sra-list_sr-bsc.txt"
+location="data/raw/sr-bsc"
+
+bash code/retrieve_datasets/run_retrieve_sra.bash  
