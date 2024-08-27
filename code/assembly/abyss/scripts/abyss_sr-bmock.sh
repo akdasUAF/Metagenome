4971001b7e98e87710c495f0e4cc2df687cd3b86
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-27 15:10:34 -0800

    Added ASbyss: slurm

--- code/assembly/abyss/scripts/abyss_sr-bmock.sh
+++ code/assembly/abyss/scripts/abyss_sr-bmock.sh
@@ -1,27 +1,25 @@
 #!/bin/env bash
-## Assembler: abyss
-## Dataset: sr-bmock
 benchmark_script="code/benchmarking/benchmark.bash"
 path_abyss="code/assembly/abyss/run_abyss.sh"
-forward_reads="data/process/sr-bmock/trimmed/sr-bmock_trimmed_1.fastq"
-reverse_reads="data/process/sr-bmock/trimmed/sr-bmock_trimmed_2.fastq"
-MAG_output="data/MAG/sr-bmock/abyss/"
-log_dir="data/analysis/sr-bmock/abyss/logs/"
+path_forward="sr-bmock/trimmed/sr-bmock_trimmed_1.fastq"
+path_reverse="sr-bmock/trimmed/sr-bmock_trimmed_2.fastq"
+path_output="data/sr-bmock/abyss/"
+path_log="data/logs/sr-bmock/abyss/"
 dataset="sr-bmock"
 task="abyss"
 
-#~~~ Abyss Only
-kmer=117
-bloom=10G
-name_assembly="${dataset}_${task}"
+######~~~~~~~~~~~~ Abyss Only
+kmer="117"
+bloom="10G"
 
-## Building a log and making a directory for results
-mkdir -p $(dirname "$MAG_output")
-mkdir -p ${log_dir}
-log_file="${log_dir}/log_asm_${task}_${dataset}.log"
+
+mkdir -p ${path_log}
+log_file="${path_log}/log_asm_${task}_${dataset}.log"
+name_assembly="${task}_${dataset}"
+
 
 # Construct the command to be executed
-command="$path_abyss"
+command="${path_abyss} ${forward_reads} ${reverse_reads} ${path_output} ${name_assembly} ${kmer} ${bloom} ${log_file}"
 
 # Execute the benchmark script with the constructed command
 bash $benchmark_script "$command" $dataset $task
