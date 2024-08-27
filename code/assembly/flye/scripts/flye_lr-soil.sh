c9a5f6e214ddd6104f7ef88f0b8d6b908d3891e4
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-27 14:47:23 -0800

    Added lr-pasture: slurm

--- code/assembly/flye/scripts/flye_lr-soil.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Dataset: Long-read Soil
-## Uses this dataset: https://www.ncbi.nlm.nih.gov/sra/SRX17069014%5Baccn
-## Assembler: Flye
-
-mkdir -p data/MAG/flye/lr-soil/
-flye --meta --nano-raw data/raw/lr-soil/SRR21053856/SRR21053856.fastq --out-dir data/MAG/flye/lr-soil --threads 12
-
