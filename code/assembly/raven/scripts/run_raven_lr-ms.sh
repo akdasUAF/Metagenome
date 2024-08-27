dcbb94210a143dd652cb7536b93cce3852e51b2e
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-27 14:41:41 -0800

    Added lr-pasture

--- code/assembly/raven/scripts/run_raven_lr-ms.sh
+++ code/assembly/raven/scripts/run_raven_lr-ms.sh
@@ -1,18 +1,20 @@
 #!/bin/env bash
 benchmark_script="code/benchmarking/benchmark.bash"
 path_raven="code/assembly/raven/run_raven.sh"
-raw_path="data/raw/lr-ms/SRR27145287/SRR27145287.fastq"
-MAG_output="data/MAG/lr-ms/raven/assembly_raven_lr-ms.fasta"
-log_dir="data/logs/lr-ms/raven"
+raw_path="data/lr-ms/raw/lr-ms_raw.fastq"
+path_output="data/lr-ms/raven/assembly_raven_lr-log.fasta"
+log_dir="data/lr-ms/logs/"
 dataset="lr-ms"
 task="raven"
 
-
+mkdir -p $(dirname "$path_output")
 mkdir -p ${log_dir}
 log_file="${log_dir}/log_asm_${task}_${dataset}.log"
 
 # Construct the command to be executed
-command="$path_raven $raw_path $MAG_output $log_file"
+command="$path_raven $raw_path $path_output $log_file"
 
 # Execute the benchmark script with the constructed command
 bash $benchmark_script "$command" $dataset $task
+
+
