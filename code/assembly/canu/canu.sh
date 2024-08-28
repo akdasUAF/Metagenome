082e2acff0ffea474766a9d3aa7660ecbd3f1b37
Bell <60406893+bwinnett12@users.noreply.github.com>
2024-08-27 23:08:09 -0800

    Final Push

--- code/assembly/canu/canu.sh
+++ code/assembly/canu/canu.sh
@@ -4,9 +4,10 @@ if [ $# -ne 4 ]; then
   echo "Usage: $0 <raw_reads> <output_directory> <genomeSize> <name>"
   exit 1
 fi
+
 raw_reads=$1
 output_directory=$2
 genomeSize=$3
 name=$4
 
-.tools/assemblers/canu-2.2/bin/canu -p name -d $output_directory genomeSize = $genomeSize -nanopore $raw_reads
+./tools/assemblers/canu-2.2/bin/canu -p name -d $output_directory genomeSize = $genomeSize -nanopore $raw_reads
