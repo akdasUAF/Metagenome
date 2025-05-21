89f40667b024a651d27e3766a0be0cc90d6e7ecc
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-05-21 20:25:49 +0700

    Updated miniasm: lr-even: Surm

--- code/assembly/miniasm/scripts/miniasm_lr-even.sh
+++ code/assembly/miniasm/scripts/miniasm_lr-even.sh
@@ -1,15 +1,17 @@
 #!/bin/env bash
 benchmark_script="code/benchmarking/benchmark.bash"
 path_miniasm="code/assembly/miniasm/run_miniasm.sh"
-reads_in="data/lr-bd/raw/lr-bd_raw.fastq"
-path_output="data/lr-bd/miniasm/"
-dataset="lr-bd"
+raw_path="data/raw/lr-even/lr-even_raw.fastq"
+path_output="data/lr-even/miniasm/"
+log_dir="data/miniasm/logs/"
+dataset="lr-even"
 task="miniasm"
 
+mkdir -p ${log_dir}
 mkdir -p $(dirname "$path_output")
 
 # Construct the command to be executed
-command="$path_miniasm $reads_in $path_output $dataset"
+command="$path_miniasm $raw_path $path_output $dataset"
 
 # Execute the benchmark script with the constructed command
 bash $benchmark_script "$command" $dataset $task
