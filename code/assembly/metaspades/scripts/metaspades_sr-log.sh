54092cfbfe4a72a6c8128bff92860381f98f47c2
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-10 15:05:53 -0800

    Added metaspades sr-ms and slurm sr-log

--- code/assembly/metaspades/scripts/metaspades_sr-log.sh
+++ code/assembly/metaspades/scripts/metaspades_sr-log.sh
@@ -1,14 +1,14 @@
 #!/bin/env bash
 benchmark_script="code/benchmarking/benchmark.bash"
 path_metaspades="code/assembly/metaspades/run_metaspades.sh"
-path_forward="data/sr-log/trimmed/sr-log_trimmed_1.fastq"
-path_reverse="data/sr-log/trimmed/sr-log_trimmed_2.fastq"
-path_output="data/sr-log/metaspades/"
-path_log="data/sr-log/logs/"
+path_forward="data/raw/sr-log/sr-log_trimmed_1.fastq"
+path_reverse="data/raw/sr-log/sr-log_trimmed_2.fastq"
+path_output="data/metaspades/sr-log/"
+path_log="data/metaspades/logs/"
 dataset="sr-log"
 task="metaspades"
 
-mkdir -p ${path_output}
+rm -rf $(dirname "$path_output")
 mkdir -p ${path_log}
 log_file="${path_log}/log_asm_${task}_${dataset}.log"
 
