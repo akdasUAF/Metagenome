fd0ed15018832421d5c5255333ab9da56a711370
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-09 17:10:02 -0800

    fixed directory creation bug

--- code/processing/fastp/scripts/fastp_sr-even.bash
+++ code/processing/fastp/scripts/fastp_sr-even.bash
@@ -10,7 +10,7 @@ reverse_trimmed_path="data/raw/sr-even/sr-even_2_trimmed.fastq"
 
 report_path="data/raw/sr-even/logs/sr-even_report.txt"
 
-mkdir -p (dirname $report_path)
+mkdir -p $(dirname "$report_path")
 
 # Execute the benchmark script with the constructed command
 fastp -i $forward_path -I $reverse_path -o $forward_trimmed_path -O $reverse_trimmed_path -h $report_path -q 20 -p 95 -l 30 -n 10
