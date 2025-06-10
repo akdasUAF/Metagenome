508d946f6c88e92b7af0db27727992a2cae9feab
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-10 14:35:06 -0800

    Added sr-log metaspades, sr-even megahit

--- code/assembly/megahit/scripts/megahit_sr-even.sh
+++ code/assembly/megahit/scripts/megahit_sr-even.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 benchmark_script="code/benchmarking/benchmark.bash"
 path_megahit="code/assembly/megahit/run_megahit.sh"
-forward_reads="data/sr-bmock/trimmed/sr-bmock_trimmed_1.fastq"
-reverse_reads="data/sr-bmock/trimmed/sr-bmock_trimmed_2.fastq"
-path_output="data/sr-bmock/megahit/"
-path_log="data/sr-bmock/logs/"
-dataset="sr-bmock"
+path_forward="data/raw/sr-even/sr-even_1_trimmed.fastq"
+path_reverse="data/raw/sr-even/sr-even_2_trimmed.fastq"
+path_output="data/megahit/sr-even/"
+path_log="data/megahit/logs/"
+dataset="sr-even"
 task="megahit"
 
 mkdir -p ${path_log}
@@ -13,7 +13,7 @@ log_file="${path_log}/log_asm_${task}_${dataset}.log"
 
 
 # Construct the command to be executed
-command="$path_megahit $forward_reads $reverse_reads $path_output $log_file"
+command="$path_megahit $path_forward $path_reverse $path_output $log_file"
 
 # Execute the benchmark script with the constructed command
 bash $benchmark_script "$command" -d "$dataset" -t "$task"
