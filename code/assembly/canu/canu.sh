b3607be484574b2e8dc0daef6d7fdc9952104328
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-10 10:48:26 -0800

    Fixed error with overhanging s in command

--- code/assembly/canu/canu.sh
+++ code/assembly/canu/canu.sh
@@ -10,4 +10,4 @@ output_directory=$2
 genomeSize=$3
 name=$4
 
-./tools/assemblers/canu-2.2/bin/canu -p $name -d $output_directory genomeSize=$genomeSize -nanopore $raw_reads useGrid=false stopOnLowCoverage=5s
+./tools/assemblers/canu-2.2/bin/canu -p $name -d $output_directory genomeSize=$genomeSize -nanopore $raw_reads useGrid=false stopOnLowCoverage=5
