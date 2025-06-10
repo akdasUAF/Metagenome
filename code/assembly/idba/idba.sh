1c6c033e49cec6c21539666e03e3e1682a78d7b4
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-10 13:52:27 -0800

    Fixed threads and added more ram to slurm: IDBA sr-even

--- code/assembly/idba/idba.sh
+++ code/assembly/idba/idba.sh
@@ -9,7 +9,5 @@ fi
 path_combined=$1
 path_output=$2
 
-
-#./tools/assemblers/idba/bin/fq2fa --merge --filter $path_forward $path_reverse $path_combined
-./tools/assemblers/idba/bin/idba_ud -r $path_combined -o $path_output
+./tools/assemblers/idba/bin/idba_ud -r $path_combined -o $path_output --num_threads 24
 
