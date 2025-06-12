7a2c0381418c4e074f967d16ccd3e254d2d5dc4a
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-11 19:09:53 -0800

    Updated IDBA

--- code/assembly/idba/idba.sh
+++ code/assembly/idba/idba.sh
@@ -9,5 +9,13 @@ fi
 path_combined=$1
 path_output=$2
 
-./tools/assemblers/idba/bin/idba_ud -r $path_combined -o $path_output --num_threads 24
+./tools/assemblers/idba/bin/idba_ud \
+    --read "$path_combined" \
+    --out "$path_output" \
+    --mink 20 \
+    --maxk 100 \
+    --step 10 \
+    --min_count 2 \
+    --num_threads 24
+
 
