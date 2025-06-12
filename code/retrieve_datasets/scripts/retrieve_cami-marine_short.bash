6485926bfef3b8226a4183651724093a71032a1a
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-12 13:49:04 -0800

    Added cami-marine_short

---
+++ code/retrieve_datasets/scripts/retrieve_cami-marine_short.bash
@@ -0,0 +1,13 @@
+mkdir -p data/raw/cami-marine_short/
+
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_0_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_0_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_1_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_1_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_2_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_2_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_3_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_3_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_4_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_4_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_5_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_5_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_6_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_6_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_7_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_7_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_8_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_8_reads.tar.gz
+wget -O ddata/raw/cami-marine_short/marmgCAMI2_sample_9_reads.tar.gz --show-progress --continue https://frl.publisso.de/data/frl:6425521/marine/short_read/marmgCAMI2_sample_9_reads.tar.gz
+
