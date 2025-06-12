bdb6a747a22eca25de2e8c276159e8d90e2a36fb
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-12 12:29:32 -0800

    Added megahit cami scripts

---
+++ code/assembly/megahit/scripts/megahit_cami-low.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+benchmark_script="code/benchmarking/benchmark.bash"
+path_megahit="code/assembly/megahit/run_megahit.sh"
+raw_reads_dir="data/raw/cami-low/"
+path_output="data/megahit/cami-low/"
+path_log="data/megahit/logs/cami-low/"
+dataset="cami-low"
+task="megahit"
+
+mkdir -p "${path_log}"
+command="$path_megahit \"$raw_reads_dir\" \"$path_output\" \"$path_log\""
+
+echo "Command to pass to benchmark.bash: ${command}"
+echo "Dataset: ${dataset}"
+echo "Task: ${task}"
+
+# Execute the benchmark script with the constructed command
+bash "$benchmark_script" "$command" "$dataset" "$task"
