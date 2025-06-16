b7249dfba0deee61ddb3f197a21a8e2ad1c28ce2
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-06-16 15:44:02 -0800

    Tweaked results retrieval script for cami

--- code/analysis/retrieve_datasets.bash
+++ code/analysis/retrieve_datasets.bash
@@ -12,9 +12,9 @@ datasets["lr-ms"]="raven flye canu miniasm"
 datasets["sr-even"]="megahit metaspades unicycler"
 datasets["sr-log"]="megahit metaspades unicycler"
 datasets["sr-ms"]="megahit metaspades"
-datasets["cami_high"]="megahit"
-datasets["cami_medium"]="megahit metaspades"
-datasets["cami_low"]="megahit"
+datasets["cami-high"]="megahit"
+datasets["cami-medium"]="megahit metaspades"
+datasets["cami-low"]="megahit"
 
 # Loop through each dataset
 for dataset in "${!datasets[@]}"; do
