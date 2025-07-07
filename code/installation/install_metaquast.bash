135513e30da57fc8e24554a661b24a280ad13a47
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-07-07 01:48:35 -0800

    Added Polishing step

--- code/installation/install_metaquast.bash
+++ code/installation/install_metaquast.bash
@@ -3,7 +3,9 @@
 ### Install Dependencies
 conda install pkg-config matplotlib gcc_linux-64 python-matplotlib
 
-
+mkdir -p tools/analysis/
 cd tools/analysis/ || exit
 git clone https://github.com/ablab/quast.git
 
+cd quast/ || exit
+./setup.py install_full
