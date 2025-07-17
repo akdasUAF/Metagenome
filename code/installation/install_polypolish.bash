f31722426516b6c8d91f7b3ab87000d050cd98ce
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-07-16 17:01:48 -0800

    Updated Medaka: 2

--- code/installation/install_polypolish.bash
+++ code/installation/install_polypolish.bash
@@ -4,7 +4,7 @@ conda create -n polypolish
 conda activate polypolish
 conda install rust
 
-
+mkdir -p tools/polishing/
 cd tools/polishing/ || exit
 
 git clone https://github.com/rrwick/Polypolish.git
