604c865443f20feb784975c45585b5ddfa65a794
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-07-17 16:51:01 -0800

    Updated megahit: Lowered memory constraints

--- code/assembly/megahit/run_megahit_all.bash
+++ code/assembly/megahit/run_megahit_all.bash
@@ -3,3 +3,10 @@ sbatch code/assembly/megahit/run_megahit.batch \
     data/raw/sr-log/test1/ \
     megahit_sr-log_assembly_run1 \
     megahit_bench_sr-log_run1
+
+
+sbatch code/assembly/megahit/run_megahit.batch  \
+    sr-log \
+    data/raw/sr-log/test1/ \
+    test1 \
+    megahit_bench_sr-log_run1
