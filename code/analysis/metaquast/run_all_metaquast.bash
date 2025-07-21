3a9b7ccc7a6f4e77db093886f74f063609197e8d
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-07-21 10:11:45 -0800

    Added metaquast wrapper

--- code/analysis/metaquast/run_all_metaquast.bash
+++ code/analysis/metaquast/run_all_metaquast.bash
@@ -4,3 +4,9 @@
 rsync -avzP ./metagenome_reference wwinnett@chinook04.alaska.edu:/center1/PANMETA/wwinnett/metagenome_data/reference/
 
 
+data/reference/sr-log/sr-log_references.txt
+
+sbatch code/analysis/metaquast/metaquast.batch \
+    "data/megahit/sr-even/test1/final.contigs.fa" \
+    "results/megahit/sr-even/test1/" \
+    "data/reference/sr-log/sr-log_references.txt"
