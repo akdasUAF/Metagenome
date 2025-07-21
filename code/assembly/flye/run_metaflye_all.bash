7ff126b8d365bc819f21c4ed252445e089a8d812
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-07-21 11:42:17 -0800

    Added flye: 2

---
+++ code/assembly/flye/run_metaflye_all.bash
@@ -0,0 +1,9 @@
+###### Lr-even
+## Test 5
+##
+sbatch code/assembly/flye/run_metaflye.batch \
+    lr-even \
+    data/raw/lr-even/test5/ \
+    test5 \
+    metaflye_bench_lr-even_run5 \
+    --nano-raw
