6aaa2edfb8303db1c735bd52628fe9d5702413bb
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-07-21 16:05:05 -0800

    Updated medaka

--- code/assembly/unicycler/run_unicycler_all.bash
+++ code/assembly/unicycler/run_unicycler_all.bash
@@ -1,5 +1,73 @@
+
+### sr-even
+
+## Run 1
+sbatch code/assembly/unicycler/run_unicycler.batch \
+    sr-even \
+    data/raw/sr-even/raw/test1/ \
+    test1 \
+    unicycler_bench_sr-even_run1
+
+
+## Run 2
 sbatch code/assembly/unicycler/run_unicycler.batch \
     sr-even \
     data/raw/sr-even/raw/test2/ \
     test2 \
     unicycler_bench_sr-even_run2
+
+
+
+
+
+
+### sr-log
+## Run 1
+sbatch code/assembly/unicycler/run_unicycler.batch \
+        sr-log \
+        data/raw/sr-log/raw/test1/ \
+        test1 \
+        unicycler_bench_sr-log_run1
+
+
+## Run 2
+sbatch code/assembly/unicycler/run_unicycler.batch \
+        sr-log \
+        data/raw/sr-log/raw/test1/ \
+        test1 \
+        unicycler_bench_sr-log_run1
+
+
+
+## Run 3
+sbatch code/assembly/unicycler/run_unicycler.batch \
+        sr-log \
+        data/raw/sr-log/raw/test1/ \
+        test1 \
+        unicycler_bench_sr-log_run1
+
+
+## Run 4
+sbatch code/assembly/unicycler/run_unicycler.batch \
+        sr-log \
+        data/raw/sr-log/raw/test1/ \
+        test1 \
+        unicycler_bench_sr-log_run1
+
+
+## Run 5
+sbatch code/assembly/unicycler/run_unicycler.batch \
+        sr-log \
+        data/raw/sr-log/raw/test1/ \
+        test1 \
+        unicycler_bench_sr-log_run1
+
+
+
+### sr-ms
+## Run 1
+sbatch code/assembly/unicycler/run_unicycler.batch \
+        sr-ms \
+        data/raw/sr-ms/raw/test1/ \
+        test1 \
+        unicycler_bench_sr-ms_run1
---
+++ code/polishing/run_medaka_all.bash
@@ -0,0 +1,5 @@
+sbatch code/polishing/run_medaka.batch \
+    lr-ms \
+    raven \
+    data/raw/lr-ms/test5/lr-ms_raw.fastq \
+    data/raven/lr-ms/test5/final.contigs.fasta
