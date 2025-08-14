eae1903254ec0b75885b5f035906e48bb2f7d066
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-08-14 10:33:31 -0800

    Added semibin2

---
+++ code/analysis/semibin2_all.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Parameters
+datasets=(lr-even lr-log lr-ms)
+assemblers=(metaflye canu raven)
+tests=(test1 test2 test3 test4 test5)
+
+for dataset in "${datasets[@]}"; do
+  for assembler in "${assemblers[@]}"; do
+    for test in "${tests[@]}"; do
+
+      contigs_fasta="data/${assembler}/${dataset}/${test}/medaka_consensus_simple_run/consensus.fasta"
+
+      # Check that inputs exist before submitting
+      if [[ ! -f "${contigs_fasta}" ]]; then
+        echo "Missing contigs FASTA: ${contigs_fasta}, skipping."
+        continue
+      fi
+
+      sbatch code/analysis/semibin2.batch \
+	  	${sample} \
+		${assembler} \
+		${run} \
+		${contigs_fasta}
+
+    done
+  done
+done
